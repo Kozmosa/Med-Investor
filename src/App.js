9e54cfe225f1de085f9529c514e21068b2f4893b
Kozmosa <xuyang1638@gmail.com>
2024-10-05 10:51:51 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ClinicalTrialDataSubmission from './components/ClinicalTrialDataSubmission';
+import './styles/index.css';
+
+const App = () => (
+  <div className="App">
+    <ClinicalTrialDataSubmission />
+  </div>
+);
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'antd/dist/antd.css'; // 引入Ant Design样式
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
