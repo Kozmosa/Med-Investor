9d9fb98a248c984970678813ca033f8b1f6d79b7
Kozmosa <xuyang1638@gmail.com>
2024-10-05 11:32:16 +0800

    修改为antd-ts脚手架

---
+++ med-investor/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Button } from 'antd';
+import './App.css';
+
+import ClinicalTrialDataSubmission from './components/CTDSubmission';
+
+function App() {
+  return (
+    <div className="App">
+      <ClinicalTrialDataSubmission />
+    </div>
+  );
+}
+
+export default App;
